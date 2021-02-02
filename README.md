843a3ee788c15a05213ce9321713945da718d58d
EnzoDaneri <enzodaneri@hotmail.com>
2021-02-02 18:34:09 -0300

    build for deploy

---
+++ README.md
@@ -0,0 +1,6 @@
+# Tour project
+
+## Project for my portfolio.
+
+- Source: https://www.youtube.com/watch?v=a_7Z7C_JCyo&t=37s
+- https://www.johnsmilga.com/
---
+++ build/precache-manifest.b61e9a2e5f976a4ff65e8a52c625fa4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "491214b6fbaca9e238b91486e16e39b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d65c5c196d28c867c0dd",
+    "url": "/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d65c5c196d28c867c0dd",
+    "url": "/static/js/main.6f56cd00.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
